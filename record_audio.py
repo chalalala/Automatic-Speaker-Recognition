c70aeace27f2cafc68f70ed2d319602077de8b61
chalalala <chalalala@pop-os.localdomain>
2020-06-06 00:26:23 +0700

    Change channel

--- record_audio.py
+++ record_audio.py
@@ -5,9 +5,8 @@ import scipy.io.wavfile as wav
 def record():
     fs=16000 # sampling rate
     duration = 5  # seconds
-    audio = sd.rec(duration * fs, samplerate=fs, channels=2,dtype='float64')
+    audio = sd.rec(duration * fs, samplerate=fs, channels=1,dtype='float64')
     print "Recording Audio"
     sd.wait()
     print "Audio recording complete"
-    sd.play(audio,fs)
     return fs,audio
