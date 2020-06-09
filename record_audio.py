987e7ee004cbcad2ef10b4b8accb38f61617428f
chalalala <chalalala@pop-os.localdomain>
2020-06-09 11:34:52 +0700

    Change datatype to int16

--- record_audio.py
+++ record_audio.py
@@ -5,7 +5,7 @@ import scipy.io.wavfile as wav
 def record():
     fs=16000 # sampling rate
     duration = 5  # seconds
-    audio = sd.rec(duration * fs, samplerate=fs, channels=1,dtype='float32')
+    audio = sd.rec(duration * fs, samplerate=fs, channels=1,dtype='int16')
     print "Recording Audio"
     sd.wait()
     print "Audio recording complete"
